fc52a425775843feaa673e60f6e3d5da0c82c7f7
vanquish233 <1505040475@qq.com>
2021-09-10 19:19:24 +0800

    finsh

--- src/main/java/com/nowcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -1,8 +1,6 @@
 package com.nowcoder.community.config;
 
-import com.nowcoder.community.interceptor.AlphaInterceptor;
-import com.nowcoder.community.interceptor.LoginRequiredInterceptor;
-import com.nowcoder.community.interceptor.LoginTicketInterceptor;
+import com.nowcoder.community.interceptor.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,18 +14,30 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
 
+//    @Autowired
+//    private LoginRequiredInterceptor loginRequiredInterceptor;
+
     @Autowired
-    private LoginRequiredInterceptor loginRequiredInterceptor;
+    private MessageInterceptor messageInterceptor;
+
+    @Autowired
+    private DataInterceptor dataInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(alphaInterceptor)    //不写下几行会拦截一切请求
-                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.jpg","/**/*.jpeg")  //这么写会排除拦截一些静态资源
-                .addPathPatterns("/register","/login")      //这么写会增加拦截的路径
-        ;
+//        registry.addInterceptor(alphaInterceptor)    //不写下几行会拦截一切请求
+//                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.jpg","/**/*.jpeg")  //这么写会排除拦截一些静态资源
+//                .addPathPatterns("/register","/login")      //这么写会增加拦截的路径
+//        ;
+        //所有请求都要拦截，除了static下的静态资源不拦截
         registry.addInterceptor(loginTicketInterceptor)
-                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.jpg","/**/*.jpeg");
-        registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+//        registry.addInterceptor(loginRequiredInterceptor)
+//                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+        registry.addInterceptor(messageInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(dataInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
--- src/main/java/com/nowcoder/community/interceptor/LoginTicketInterceptor.java
+++ src/main/java/com/nowcoder/community/interceptor/LoginTicketInterceptor.java
@@ -6,6 +6,10 @@ import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CookieUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,6 +39,11 @@ public class LoginTicketInterceptor implements HandlerInterceptor {
                 User user = userService.findUserById(loginTicket.getUserId());
                 //在本次请求中持有用户
                 hostHolder.setUser(user);
+                // 查询用户的权限,并存入SecurityContext,以便于Security进行授权。
+                //使用拦截器验证用户的权限
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        user, user.getPassword(), userService.getAuthorities(user.getId()));
+                SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
             }
         }
         return true;
@@ -50,5 +59,6 @@ public class LoginTicketInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         hostHolder.clear();
+        SecurityContextHolder.clearContext();
     }
 }
